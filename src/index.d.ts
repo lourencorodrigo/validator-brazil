d5c8eb3653efa685b2e76be7b0e1d15c70f17679
Rodrigo Lourenço <rsilvape@gmail.com>
2018-12-27 12:52:05 -0300

    Validação de CPF

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,3 +1,7 @@
 /*~ Retorna true se o cnpj estiver válido.
  */
 export function isCnpj(cnpj: string): boolean;
+
+/*~ Retorna true se o cpf estiver válido.
+ */
+export function isCpf(cpf: string): boolean;
