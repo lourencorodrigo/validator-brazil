c67e59ae9a0a2c5257450583d43f228ef478734d
Rodrigo Lourenço <rsilvape@gmail.com>
2019-01-02 22:21:24 -0300

    Testes unitários

--- src/tests/cnpj.spec.js
+++ src/tests/cnpj.spec.js
@@ -1,15 +1,23 @@
-import { isCnpj } from '../index';
+import { isCnpj } from "../index";
 
-describe('cnpj validation', () => {
-    it('should return true to valid cnpj', () => {
-        expect(isCnpj('00933180000164')).toEqual(true);
-    });
+describe("cnpj validation", () => {
+  it("should return true to valid cnpj without hyphen and points", () => {
+    expect(isCnpj("00933180000164")).toEqual(true);
+  });
 
-    it('should return false to invalid cnpj - 1', () => {
-        expect(isCnpj('001112220000133')).toEqual(false);
-    });
+  it("should return true to valid cnpj with hyphen and points", () => {
+    expect(isCnpj("00.933.180/0001-64")).toEqual(true);
+  });
 
-    it('should return false to invalid cnpj - 2', () => {
-        expect(isCnpj('00000000000000')).toEqual(false);
-    });
+  it("should return false with letters", () => {
+    expect(isCnpj("00933180000164a")).toEqual(false);
+  });
+
+  it("should return false to invalid cnpj with hyphen and points", () => {
+    expect(isCnpj("001112220000133")).toEqual(false);
+  });
+
+  it("should return false to invalid cnpj", () => {
+    expect(isCnpj("00000000000000")).toEqual(false);
+  });
 });
--- src/tests/cpf.spec.js
+++ src/tests/cpf.spec.js
@@ -1,15 +1,23 @@
-import { isCpf } from '../index';
+import { isCpf } from "../index";
 
-describe('cpf validation', () => {
-    it('should return true to valid cpf', () => {
-        expect(isCpf('14552586017')).toEqual(true);
-    });
+describe("cpf validation", () => {
+  it("should return true to valid cpf wythout hyphen and points", () => {
+    expect(isCpf("14552586017")).toEqual(true);
+  });
 
-    it('should return false to invalid cpf - 1', () => {
-        expect(isCpf('00011122233')).toEqual(false);
-    });
+  it("should return true to valid cpf with hyphen and points", () => {
+    expect(isCpf("145.525.860-17")).toEqual(true);
+  });
 
-    it('should return false to invalid cpf - 2', () => {
-        expect(isCpf('00011122233')).toEqual(false);
-    });
+  it("should return false with letters", () => {
+    expect(isCpf("14552586017a")).toEqual(false);
+  });
+
+  it("should return false to invalid cpf wythout hyphen and points", () => {
+    expect(isCpf("00011122233")).toEqual(false);
+  });
+
+  it("should return false to invalid cpf with hyphen and points", () => {
+    expect(isCpf("000.111.222-33")).toEqual(false);
+  });
 });
